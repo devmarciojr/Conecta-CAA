96a66c79a0bc16fbc037087a992600ad6aa1b35e
Marcio Júnior <73200722+devmarciojr@users.noreply.github.com>
2025-04-02 00:24:37 -0300

    Update settings.py

--- conectacaa/conectacaa/settings.py
+++ conectacaa/conectacaa/settings.py
@@ -31,7 +31,18 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+# Configuração de hosts permitidos
+ALLOWED_HOSTS = [
+    'conecta-caa.onrender.com',
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0',
+]
+
+# Configuração para CSRF
+CSRF_TRUSTED_ORIGINS = [
+    'https://conecta-caa.onrender.com',
+]
 
 
 # Application definition
